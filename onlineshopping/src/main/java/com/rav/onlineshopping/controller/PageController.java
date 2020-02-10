ef58304ada9e995e267dd9f6796fa828d0d3e781
ravi margaveni <mravi.m123@gmail.com>
2020-02-10 18:10:13 +0530

    commits

--- onlineshopping/src/main/java/com/rav/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/rav/onlineshopping/controller/PageController.java
@@ -1,7 +1,11 @@
 package com.rav.onlineshopping.controller;
 
+import javax.ws.rs.Path;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -9,7 +13,27 @@ public class PageController {
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "welcome to spring web mvc"); 
+		mv.addObject("greeting", "welcome to spring web mvc");
 		return mv;
 	}
+
+//
+//	@RequestMapping(value = "/test")
+//	public ModelAndView test(@RequestParam(value = "greeting", required = false) String greeting) {
+////	if (greeting==null) {
+////		greeting="hello there";
+////	}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
+//
+//	@RequestMapping(value = "/path/{myname}")
+//	public ModelAndView getUserName(@PathVariable(value = "myname") String myName) {
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", myName);
+//		return mv;
+//
+//	}
+//
 }
--- onlineshopping/src/main/webapp/WEB-INF/views/page.jsp
+++ onlineshopping/src/main/webapp/WEB-INF/views/page.jsp
@@ -1,12 +1,18 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c"  uri="http://java.sun.com/jsp/jstl/core"%>
+
+<c:set var="contextRoot" value="${pageContext.request.contextPath }"/>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>online shopping</title>
 </head>
 <body>
-${greeting}
+<h1 style="color:green">
+
+${contextRoot} says-- ${greeting}
+</h1>
 </body>
 </html>
